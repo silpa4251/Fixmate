c8bf0d16b17e01a63b950085bdedc958f9911ab9
silpa4251 <silpadas4251@gmail.com>
2025-01-19 11:51:11 +0530

    error handled

--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -1,23 +1,25 @@
 const {registerValidation,loginValidation,} = require("../validations/userValidations");
 const { registerUser, loginUser} = require("../services/authService");
+const asyncErrorHandler = require("../utils/asyncErrorHandler")
+const CustomError = require("../utils/customError");
 
 // Registering a new user
-const register = async (req, res) => {
+const register = asyncErrorHandler(async (req, res) => {
   const { error } = registerValidation(req.body)
   if (error) throw new CustomError(error.details[0].message, 400);
 
   const result = await registerUser(req.body);
-  res.status(201).json({ status: "success", ...result})
-};
+  res.status(201).json({ status: "success", result})
+});
 
 
 //Login a user
-const login = async (req, res) => {
+const login = asyncErrorHandler(async (req, res) => {
   const {error} = loginValidation(req.body);
   if (error) throw new CustomError(error.details[0].message, 400);
 
   const result = await loginUser(req.body);
   res.status(200).json({ status: "success",result})
-};
+});
 
 module.exports = { register, login };
